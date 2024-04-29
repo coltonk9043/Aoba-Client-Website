27274ff3a68b7b187b674c57c54e1c6dc308b280
Jonathan Nanno <jon.nanno@gmail.com>
2024-04-29 14:29:11 -0400

    Changed imports for new Titlebar component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,10 @@
-import { TitleBar } from "@/components/TitleBar";
 import Form from "@/components/Form";
-import TestComponent from "@/components/TestComponent";
+import TitleBar from "@/components/TitleBar";
 
 export default function Page() {
     return (
         <main>
-            <TestComponent />
+            <TitleBar />
            <Form />
         </main>
     )
--- app/wiki/page.tsx
+++ app/wiki/page.tsx
@@ -1,9 +1,10 @@
-import { TitleBar } from "@/components/TitleBar";
+import TitleBar from "@/components/TitleBar";
+
 
 export default function Page() {
     return (
         <main>
-            <TitleBar/>
+            <TitleBar />
 
             <h1 className="text-center m-[100px]">COMING SOON!</h1>
         </main>
