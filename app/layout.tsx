a294187284e8e05a4ac17c80a842f2f5eae4de26
Colton <coltonk9043@yahoo.ca>
2024-04-29 15:05:11 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.ico" sizes="any" />
       </head>
       <body className="bg-zinc-900">{children}</body>
     </html>
