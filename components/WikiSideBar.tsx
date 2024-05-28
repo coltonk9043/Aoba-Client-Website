7bc9f6556813355a3ef340df0ebeecbae1451923
Colton John Kennedy <ckenne87@uwo.ca>
2024-05-28 19:41:23 -0400

    Attempt to fix Wiki sidebar

--- components/WikiSideBar.tsx
+++ components/WikiSideBar.tsx
@@ -1,5 +1,6 @@
-'use server'
-import fs from "node:fs";
+'use client'
+import { WikiEntries, WikiEntry } from "@/app/wiki/wiki";
+import React from "react";
 
 export type DirectoryTreeNode = {
     name: string,
@@ -7,88 +8,45 @@ export type DirectoryTreeNode = {
     children: Array<DirectoryTreeNode>
 }
 
-export async function WikiSideBar({ posts } : { posts?: string[]}) {
-    const pages : string[] = fs
-        .readdirSync("app\\wiki", { withFileTypes: true, recursive: true })
-        .filter((file) => file.isDirectory())
-        .map((folder) => folder.path.replace('/', '\\') + "\\" + folder.name)
-        .map((folder) => folder.replace("app\\", "").replace("_", " "))
-          
-    console.log(pages)
-    const tree = {name: "wiki", url_path: "wiki", children: []};
+export const CollapsibleTree =  (props : {entry : WikiEntry}) => {
+    const [expanded, setExpanded] = React.useState<boolean>(true);
 
-    const addPath = (path : string, tree : DirectoryTreeNode) => {
-        // helper function to create child objects
-        const createChild = (name : string, url_path: string) => ({
-          name,
-          url_path,
-          children: []
-        });
-      
-        // split path to array of folders and files
-        const parts = path.split("\\");
-      
-        // create tree if empty
-        if (!tree.name) {
-          Object.assign(tree, createChild(parts[0], "/wiki/" + path));
-        }
-      
-        // check if root folder is correct
-        if (tree.name !== parts[0]) {
-          throw new Error(`Root folder is not "${tree.name}"`);
-        }
-        parts.shift();
-      
-        // check and add other path parts
-        parts.reduce((current, p) => {
-          const child = current.children.find((child) => child.name === p);
-          if (child) {
-            return child;
-          }
-      
-          const newChild = createChild(p, "/" + path);
-          current.children.push(newChild);
-          return newChild;
-        }, tree);
-    };
-      
-    pages.forEach((path) => addPath(path, tree));
-
-    function capitalizeFirstLetter(string : string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    const GenerateTreeDOM = (tree : DirectoryTreeNode) => {
-        const mapChildren = (childrenList : Array<DirectoryTreeNode> ) => {
-            return childrenList.map((e, index) => {
-                return GenerateTreeDOM(e)
-            })
-        }
-
-        if(Array.isArray(tree.children)){
-            if(tree.children.length <= 0){
-                return (<a href={tree.url_path}><p>{capitalizeFirstLetter(tree.name)}</p></a>)
+    const mapChildren = (childrenList : WikiEntry[] ) => {
+        return childrenList.map((e, index) => {
+            if(Array.isArray(e.children)){
+                if(e.children.length == undefined){
+                    return (<a key={index} href={"/" + e.path}><p>{e.name}</p></a>)
+                }else{
+                    return (<CollapsibleTree key={index} entry={e}/>)
+                }
             }else{
-                return (
-                    <>
-                        <p className="font-semibold text-xl">{capitalizeFirstLetter(tree.name)}</p>
-                        <div className="ml-[3rem]">
-                            {mapChildren(tree.children)}
-                        </div>
-                    </>
-                    
-                )
+                return (<a key={index} href={"/" + e.path}><p>{e.name}</p></a>)
             }
-        }else{
-            return (<a href={tree.url_path}><p>{capitalizeFirstLetter(tree.name)}</p></a>)
+        })
+    }
+
+    const renderChildren = () => {
+        if(props.entry.children !== undefined && expanded){
+            return(
+                <div className="ml-[3rem]">
+                    {mapChildren(props.entry.children)}
+                </div>
+            )
         }
     }
-      
 
-    return(
-        <div className="p-5 bg-zinc-800 border-r border-zinc-500 w-auto">
-            {GenerateTreeDOM(tree)}
-        </div>
+    return (
+        <>
+        <p className="font-semibold text-xl" onClick={(e) => {setExpanded(!expanded)}}>{props.entry.name}</p>
+        {renderChildren()}
+        </>
     )
 }
 
+export async function WikiSideBar({ posts } : { posts?: string[]}) {
+    return(
+        <div className="p-5 bg-zinc-800 border-r border-zinc-500 w-auto">
+            <CollapsibleTree entry={WikiEntries} />
+        </div>
+    )
+}
